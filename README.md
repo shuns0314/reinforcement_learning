8685f471c684157836f5568adbf49215c468d616
shuns0314 <shunsuke.naka0314@gmail.com>
2021-01-01 22:13:58 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # study_reinforcement_learning
 # reinforcement_learning
+
+## 環境
+
+- manjaro linux59
+- nvidia-docker
+
+## 手順
+host PCではじめに以下のコマンドを実行。
+
+    host localhost:
+
+## 参考
+- https://unskilled.site/docker%E3%82%B3%E3%83%B3%E3%83%86%E3%83%8A%E3%81%AE%E4%B8%AD%E3%81%A7gui%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%82%92%E8%B5%B7%E5%8B%95%E3%81%95%E3%81%9B%E3%82%8B/
+- https://qiita.com/eisoku9618/items/c2cca0f0bf764def2efd
--- src/random_cartpole.py
+++ src/random_cartpole.py
@@ -4,9 +4,6 @@ import gym
 
 from matplotlib import animation
 
-ENV = "CartPole-v0"
-NUM_DIZITIZED = 6
-
 
 def display_frames_as_gif(frames):
     plt.figure(figsize=(frames[0].shape[0] / 72.0, frames[0].shape[0] / 72.0))
@@ -27,7 +24,7 @@ def display_frames_as_gif(frames):
 
 def random_move():
     frames = []
-    env = gym.make(ENV)
+    env = gym.make("CartPole-v0")
     observation = env.reset()
 
     for step in range(200):
@@ -35,21 +32,6 @@ def random_move():
         action = np.random.choice(2)
         observation, reward, done, info = env.step(action)
     return frames
-
-
-def bins(clip_min, clip_max, num):
-    return np.linspace(clip_min, clip_max, num + 1)[1:-1]
-
-
-def digitize_state(observation):
-    cart_pos, cart_v, pole_angle, pole_v = observation
-    digitized = [
-        np.digitize(cart_pos, bins=bins(-2.4, 2.4, NUM_DIZITIZED)),
-        np.digitize(cart_v, bins=bins(-3.0, 3.0, NUM_DIZITIZED)),
-        np.digitize(pole_angle, bins=bins(-0.5, 0.5, NUM_DIZITIZED)),
-        np.digitize(pole_v, bins=bins(-2.0, 2.0, NUM_DIZITIZED)),
-    ]
-    return sum([x * (NUM_DIZITIZED ** i) for i, x in enumerate(digitized)])
 
 
 def main():

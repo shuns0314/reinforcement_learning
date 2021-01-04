15b88e171780a1bf36ffadaa4126ca292d014627
shuns0314 <shunsuke.naka0314@gmail.com>
2021-01-04 10:43:11 +0000

    update

--- src/dueling_network_cartpole.py
+++ src/dueling_network_cartpole.py
@@ -33,7 +33,7 @@ def display_frames_as_gif(frames):
         plt.gcf(), animate, frames=len(frames), interval=50
     )
 
-    anim.save("movie_cartpole_DDQN.gif")
+    anim.save("gif/movie_cartpole_duerling_network.gif")
 
 
 class ReplayMemory:
@@ -78,12 +78,18 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(n_in, n_mid)
         self.fc2 = nn.Linear(n_mid, n_mid)
-        self.fc3 = nn.Linear(n_mid, n_out)
+        self.fc3_adv = nn.Linear(n_mid, n_out)
+        self.fc3_v = nn.Linear(n_mid, 1)
 
     def forward(self, x):
         h1 = F.relu(self.fc1(x))
         h2 = F.relu(self.fc2(h1))
-        return self.fc3(h2)
+
+        adv = self.fc3_adv(h2)
+        val = self.fc3_v(h2).expand(-1, adv.size(1))
+
+        output = val + adv - adv.mean(1, keepdim=True).expand(-1, adv.size(1))
+        return output
 
 
 class Brain:
